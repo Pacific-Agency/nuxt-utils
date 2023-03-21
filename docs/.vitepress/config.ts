d8d7b007968a2451943d603c0ea6f7ab8b90bec2
Vasily Kuzin <exer7um@gmail.com>
2023-03-21 15:28:01 +0300

    Настройка Vitepress #22

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vitepress"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/nuxt-utils/",
+  description: "Утилиты Nuxt от Pacific Agency",
+  lang: "ru-RU",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { link: "/", text: "Главная" },
+      { link: "/components/map-widget", text: "Компоненты" },
+      { link: "/composables/use-phone-formatter", text: "Composables" },
+    ],
+    sidebar: [
+      {
+        items: [{ link: "/components/map-widget", text: "MapWidget" }],
+        text: "Компоненты",
+      },
+      {
+        items: [
+          {
+            link: "/composables/use-phone-formatter",
+            text: "usePhoneFormatter",
+          },
+          {
+            link: "/composables/use-price-formatter",
+            text: "usePriceFormatter",
+          },
+          {
+            link: "/composables/use-scroll-lock",
+            text: "useScrollLock",
+          },
+        ],
+        text: "Composables",
+      },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/Pacific-Agency/nuxt-utils" },
+    ],
+  },
+  title: "Утилиты Nuxt",
+})
