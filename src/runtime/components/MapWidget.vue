3b9f51a8919d0faf48b47d4b52cd9a4a88324d40
Vasily Kuzin <exer7um@gmail.com>
2023-03-21 12:18:17 +0300

    Компонент Яндекс Карт #21

---
+++ src/runtime/components/MapWidget.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  /** `ID` виджета Яндекс.Карт */
+  widgetId: string | undefined
+}>()
+</script>
+
+<template>
+  <iframe
+    allowfullscreen="true"
+    class="map-widget"
+    height="100%"
+    :src="`https://yandex.ru/map-widget/v1/-/${widgetId}`"
+    width="100%"
+  ></iframe>
+</template>
+
+<style scoped>
+.map-widget {
+  border: none;
+}
+</style>
