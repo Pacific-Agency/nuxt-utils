19b92b605d6dc7064088c0862de51525f0293b86
ExEr7um <ExEr7um@users.noreply.github.com>
2023-04-23 19:57:52 +0000

    Deploying to gh-pages from @ Pacific-Agency/nuxt-utils@6594b52d819f3d5f58d9975b59cdbe9d88528619 🚀

---
+++ assets/installation.md.ac9ad282.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,o as s,c as n,O as l}from"./chunks/framework.52de2457.js";const h=JSON.parse('{"title":"Установка и настройка","description":"","frontmatter":{},"headers":[],"relativePath":"installation.md"}'),e={name:"installation.md"},o=l("",7),t=[o];function p(c,i,r,d,y,C){return s(),n("div",null,t)}const D=a(e,[["render",p]]);export{h as __pageData,D as default};
