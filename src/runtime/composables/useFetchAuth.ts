2d97be127ef6a34666e4a293cc0716552ade17f0
Vasily Kuzin <exer7um@gmail.com>
2023-04-25 14:12:42 +0300

    Рефактор функции #72

--- src/runtime/composables/useFetchAuth.ts
+++ src/runtime/composables/useFetchAuth.ts
@@ -1,68 +1,37 @@
-import { useCookie, useRequestHeaders } from "#imports"
-import type { H3Event } from "h3"
-import { appendHeader } from "h3"
+import { useCookie, useFetch, useRequestHeaders } from "#imports"
 
-/** Параметры запроса */
-type FetchParams = Parameters<typeof $fetch.raw>
+/** Параметры запроса от useFetch */
+type FetchParams<TResponse> = Parameters<typeof useFetch<TResponse>>
 
 /**
  * Функция для работы с эдпоинтами с авторизацией.
  *
- * Решает 3 задачи:
+ * Решает 2 задачи:
  * - Проксит `cookie` клиента в серверные запросы при `SSR`
- * - Проксит `cookie` из серверных запросов на клиент
  * - Добавляет заголовок `X-CSRFToken` со значением из `cookie` `csrftoken`
  * @param url - `URL` для запроса
- * @param event - событие запроса `h3`
  * @param options - параметры запроса
- * @returns Объект с параметрами `data` и `error`
+ * @returns Ответ от API, формат как у useFetch
  * @see https://pacific-agency.github.io/nuxt-utils/composables/use-fetch-auth.html
  * @example
  * ```ts
- * const event = useRequestEvent()
- *
- * const { data } = await useFetchAuth("/auth/csrf/", event, {
+ * const { data, error } = await useFetchAuth<Response>("/auth/csrf/", {
  *   baseURL: useRuntimeConfig().public.apiBase
  * })
  * ```
  */
 export default async function <TResponse>(
-  url: FetchParams[0],
-  event?: H3Event,
-  options?: FetchParams[1]
+  url: FetchParams<TResponse>[0],
+  options?: FetchParams<TResponse>[1]
 ) {
-  try {
-    /** Ответ от эндпоинта */
-    const response = await $fetch.raw<TResponse>(url, {
-      credentials: "include",
-      headers: {
-        // Выставляем `csrftoken` из `cookie` в заголовок
-        "X-CSRFToken": useCookie("csrftoken").value ?? "",
-        // Проксим `cookie` клиента на сервер
-        cookie: useRequestHeaders(["cookie"]).cookie ?? "",
-      },
-      ...options,
-    })
-
-    /** `cookie` из ответа */
-    const cookies = (response.headers.get("set-cookie") ?? "")
-      .replace(/expires=.[^;]*;/g, "") // Удаление `expires`, чтобы правильно сработал `split`. Это можно сделать, так как передается `maxAge`
-      .split(", ") // Разбиение полученной строки на отдельные `cookie`
-
-    // Запись `cookie` в ответ
-    for (const cookie of cookies) {
-      // На клиенте event будет `undefined`, поэтому присутствует проверка на это
-      if (event) {
-        // Добавляем `cookie` в ответ
-        appendHeader(event, "set-cookie", cookie)
-      }
-    }
-
-    // Возвращаем ответ от API
-    return { data: response._data }
-  } catch (error) {
-    // Ловим ошибку
-    console.error(error) // Выводим в консоль
-    return { error: error } // Возвращаем в объект `error`
-  }
+  return useFetch<TResponse>(url, {
+    credentials: "include",
+    headers: {
+      // Выставляем `csrftoken` из `cookie` в заголовок
+      "X-CSRFToken": useCookie("csrftoken").value ?? "",
+      // Проксим `cookie` клиента на сервер
+      cookie: useRequestHeaders(["cookie"]).cookie ?? "",
+    },
+    ...options,
+  })
 }
