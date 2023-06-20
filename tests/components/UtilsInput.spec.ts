39fdbd40bad6c36145455f161a3392233ad9f1cf
Maxim Kuzin <iskait04@gmail.com>
2023-06-21 02:40:56 +0300

    Тесты #119

--- tests/components/UtilsInput.spec.ts
+++ tests/components/UtilsInput.spec.ts
@@ -126,6 +126,44 @@ describe.concurrent("Компонент UtilsInput", () => {
     expect(input.attributes("disabled")).toBe("")
   })
 
+  test("Выставление параметра `pattern`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInput, {
+      props: {
+        pattern: "\\+\\d \\d{3} \\d{3}-\\d{2}-\\d{2}",
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка атрибута `pattern`
+    expect(input.attributes("pattern")).toBe(
+      "\\+\\d \\d{3} \\d{3}-\\d{2}-\\d{2}"
+    )
+  })
+
+  test("Выставление параметров `maxlength` и `minlength`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInput, {
+      props: {
+        maxlength: 12,
+        minlength: 4,
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка существования атрибута `maxlength`.
+    expect(input.attributes("maxlength")).toBe("12")
+
+    // Проверка существования атрибута `minlength`.
+    expect(input.attributes("minlength")).toBe("4")
+  })
+
   test("Параметр `required` по умолчанию `true`", () => {
     /** Компонент */
     const wrapper = mount(UtilsInput, {
@@ -183,6 +221,21 @@ describe.concurrent("Компонент UtilsInput", () => {
 
     // Проверка значения атрибута `type`.
     expect(input.attributes("type")).toBe("password")
+  })
+
+  test("Работа `v-maska`", async () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInput, {
+      props: {
+        dataMaska: "###-###",
+        ...defaultProps,
+      },
+    })
+    // Выставляем значение инпута
+    await wrapper.find("input").setValue("1234asdas56789545")
+
+    // Проверяем значение инпута
+    expect(wrapper.find("input").element.value).toBe("123-456")
   })
 
   test("Работа `v-model`", async () => {
