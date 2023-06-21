547f98e8891c44e73d85bcde9db7502f92212109
Vasily Kuzin <exer7um@gmail.com>
2023-06-22 00:28:42 +0300

    Компонент UtilsInputMasked #121

---
+++ src/runtime/components/UtilsInputMasked.vue
@@ -0,0 +1,106 @@
+<!-- eslint-disable jsdoc/check-tag-names -->
+<script setup lang="ts">
+import { vMaska } from "maska";
+
+withDefaults(
+  defineProps<{
+    /**
+     * Является ли поле выключенным.
+     *
+     * Задает параметр `disabled`.
+     */
+    disabled?: boolean,
+    /**
+     * Идентификатор поля ввода.
+     *
+     * Устанавливает `id` и `name`, которые используются в `formData`.
+     */
+    id: string,
+    /**
+     * Подпись поля ввода.
+     *
+     * Создает `label` и задает ему необходимый `id`.
+     */
+    label?: string, // eslint-disable-line vue/require-default-prop
+    /**
+     * Позволяет задать маску, которой будет соответствовать значение элемента формы
+     *
+     * Устанавливает атрибут `data-maska` у `input`.
+     */
+    mask: string,
+    /**
+     * Ограничитель максимального количества символов.
+     *
+     * Устанавливает атрибут `maxlength` у `input`.
+     */
+    maxlength?: number // eslint-disable-line vue/require-default-prop
+    /**
+     * Ограничитель минимального количества символов.
+     *
+     * Устанавливает атрибут `minlength` у `input`.
+     */
+    minlength?: number // eslint-disable-line vue/require-default-prop
+    /**
+     * Регулярное выражение, которому должно соответствовать значение у `input`.
+     *
+     * Устанавливает атрибут `pattern` у `input`.
+     */
+    pattern?: string // eslint-disable-line vue/require-default-prop
+    /**
+     * Плейсхолдер для поля.
+     *
+     * Задает параметр `placeholder`.
+     */
+    placeholder?: string // eslint-disable-line vue/require-default-prop
+    /**
+     * Является ли поле обязательным для заполнения.
+     *
+     * Выставляет атрибут `required`.
+     *
+     * @defaultValue `true`
+     */
+    required?: boolean
+    /**
+     * Тип поля.
+     *
+     * @defaultValue `text`
+     */
+    type?: "number" | "text"
+  }>(),
+  {
+    required: true,
+    type: "text",
+  }
+)
+
+/** Текущий текст поля */
+const modelValue = defineModel<number | string>({
+  local: true,
+})
+</script>
+
+<template>
+  <div class="utils-input-masked-container utils-form-container">
+    <label
+      v-if="label"
+      class="utils-input-masked-label utils-form-label"
+      :for="id"
+      v-text="label"
+    />
+    <input
+      :id="id"
+      v-model="modelValue"
+      v-maska
+      class="utils-input-masked-input utils-form-input"
+      :data-maska="mask"
+      :disabled="disabled"
+      :maxlength="maxlength"
+      :minlength="minlength"
+      :name="id"
+      :pattern="pattern"
+      :placeholder="placeholder"
+      :required="required"
+      :type="type"
+    />
+  </div>
+</template>
---
+++ tests/components/UtilsInputMasked.spec.ts
@@ -0,0 +1,353 @@
+import { mount } from "@vue/test-utils"
+import { describe, expect, test } from "vitest"
+
+import UtilsInputMasked from "@/runtime/components/UtilsInputMasked.vue"
+
+describe.concurrent("Компонент UtilsInputMasked", () => {
+  const defaultProps = {
+    id: "test-field",
+    mask: "##-##"
+  }
+
+  test("Рендер", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: defaultProps,
+    })
+
+    // Проверяем рендер компонента
+    expect(wrapper.element).toBeTruthy()
+  })
+
+  test("Выставление атрибутов `id` и `name` на поле ввода", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: defaultProps,
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка атрибута `id`
+    expect(input.attributes("id")).toBe(defaultProps.id)
+    // Проверка атрибута `name`
+    expect(input.attributes("name")).toBe(defaultProps.id)
+  })
+
+  test("Отсутствие `label` в разметке без передачи пропса", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: defaultProps,
+    })
+
+    /** Элемент `label` */
+    const label = wrapper.find("label")
+
+    // Проверка существования `label`
+    expect(label.exists()).toBeFalsy()
+  })
+
+  test("Выставление атрибута `for` на `label`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        label: "Тестовый label",
+        ...defaultProps,
+      },
+    })
+
+    /** Элемент `label` */
+    const label = wrapper.find("label")
+
+    // Проверка атрибута `for`
+    expect(label.attributes("for")).toBe(defaultProps.id)
+  })
+
+  test("Выставление текста `label`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        label: "Тестовый label",
+        ...defaultProps,
+      },
+    })
+
+    /** Элемент `label` */
+    const label = wrapper.find("label")
+
+    // Проверка атрибута `for`
+    expect(label.element.textContent).toBe("Тестовый label")
+  })
+
+  test("Выставление текста `placeholder`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        placeholder: "Тестовый placeholder",
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка атрибута `placeholder`
+    expect(input.attributes("placeholder")).toBe("Тестовый placeholder")
+  })
+
+  test("Параметр `disabled` по умолчанию false", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка отсутствия атрибута `disabled`.
+    expect(input.attributes("disabled")).toBeUndefined()
+  })
+
+  test("Выставление параметра `disabled`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        disabled: true,
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка существования атрибута `disabled`.
+    // При преобразовании в HTML, true заменяется на пустую строку.
+    expect(input.attributes("disabled")).toBe("")
+  })
+
+  test("Выставление параметра `pattern`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        pattern: "\\+\\d \\d{3} \\d{3}-\\d{2}-\\d{2}",
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка атрибута `pattern`
+    expect(input.attributes("pattern")).toBe(
+      "\\+\\d \\d{3} \\d{3}-\\d{2}-\\d{2}"
+    )
+  })
+
+  test("Выставление параметров `maxlength` и `minlength`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        maxlength: 12,
+        minlength: 4,
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка существования атрибута `maxlength`.
+    expect(input.attributes("maxlength")).toBe("12")
+
+    // Проверка существования атрибута `minlength`.
+    expect(input.attributes("minlength")).toBe("4")
+  })
+
+  test("Параметр `required` по умолчанию `true`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: defaultProps,
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка существования атрибута `required`.
+    // При преобразовании в HTML, true заменяется на пустую строку.
+    expect(input.attributes("required")).toBe("")
+  })
+
+  test("Выставление параметра `required`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        required: false,
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка отсутствия атрибута `required`.
+    expect(input.attributes("required")).toBeUndefined()
+  })
+
+  test("Параметр `type` по умолчанию `text`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: defaultProps,
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка значения атрибута `type`.
+    expect(input.attributes("type")).toBe("text")
+  })
+
+  test("Выставление параметра `type`", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        type: "password",
+        ...defaultProps,
+      },
+    })
+
+    /** Текстовое поле */
+    const input = wrapper.find("input")
+
+    // Проверка значения атрибута `type`.
+    expect(input.attributes("type")).toBe("password")
+  })
+
+  test("Работа `v-maska`", async () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        ...defaultProps,
+        mask: "###-###",
+      },
+    })
+
+    // Выставляем значение инпута
+    await wrapper.find("input").setValue("1234asdas56789545")
+
+    // Проверяем значение инпута
+    expect(wrapper.find("input").element.value).toBe("123-456")
+  })
+
+  test("Работа `v-model`", async () => {
+    const parentComponent = mount({
+      components: {
+        UtilsInputMasked,
+      },
+      data() {
+        return {
+          text: "",
+        }
+      },
+      template: `
+        <UtilsInputMasked mask="##-##" id="test-field" v-model="text" />
+      `,
+    })
+
+    /** Текстовое поле */
+    const input = parentComponent.find("input")
+
+    // Выставляем значение инпута
+    await input.setValue("1234")
+
+    // Проверяем значение переменной
+    expect(parentComponent.vm.text).toBe("12-34")
+
+    // Выставляем значение переменной
+    await parentComponent.setData({
+      text: "43-21",
+    })
+
+    // Проверяем значение инпута
+    expect(input.element.value).toBe("43-21")
+  })
+
+  test("Работа через `formData`", async () => {
+    const parentComponent = mount({
+      components: {
+        UtilsInputMasked,
+      },
+      data() {
+        return {
+          formData: new FormData(),
+        }
+      },
+      methods: {
+        submit(event: Event) {
+          /** Форма */
+          const form = event.target as HTMLFormElement
+
+          /** Объект со всеми данными формы */
+          this.formData = new FormData(form)
+        },
+      },
+      template: `
+        <form @submit="submit">
+          <UtilsInputMasked mask="##-##" id="test-field" />
+        </form>
+      `,
+    })
+
+    /** Текстовое поле */
+    const input = parentComponent.find("input")
+
+    /** Форма с полями */
+    const form = parentComponent.find("form")
+
+    // Выставляем значения поля
+    await input.setValue("1234")
+
+    // Триггерим отправку формы
+    await form.trigger("submit")
+
+    /** Данные формы */
+    const formData = Object.fromEntries(parentComponent.vm.formData.entries())
+
+    // Проверяем, что правильно выставлены все данные
+    expect(formData).toStrictEqual({
+      "test-field": "12-34",
+    })
+  })
+
+  test("Наличие классов для стилизации", () => {
+    /** Компонент */
+    const wrapper = mount(UtilsInputMasked, {
+      props: {
+        label: "Тестовый label",
+        ...defaultProps,
+      },
+    })
+
+    /** Классы `wrapper` */
+    const wrapperClasses = wrapper.classes()
+    /** Классы `label` */
+    const labelClasses = wrapper.find("label").classes()
+    /** Классы `input` */
+    const inputClasses = wrapper.find("input").classes()
+
+    // Глобальный класс контейнера
+    expect(wrapperClasses).toContain("utils-form-container")
+    // Глобальный класс `label`
+    expect(labelClasses).toContain("utils-form-label")
+    // Глобальный класс поля ввода
+    expect(inputClasses).toContain("utils-form-input")
+
+    // Проверка классов контейнера
+    expect(wrapperClasses).toContain("utils-input-masked-container")
+    // Проверка классов у `label`
+    expect(labelClasses).toContain("utils-input-masked-label")
+    // Проверка классов у `input`
+    expect(inputClasses).toContain("utils-input-masked-input")
+  })
+})
