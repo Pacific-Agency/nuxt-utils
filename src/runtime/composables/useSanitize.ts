60b6107bb4480a9e86b0fc369774db4170e8923d
Vasily Kuzin <exer7um@gmail.com>
2023-10-20 19:13:10 +0300

    feat: Функция `useSanitize` (#165)
    * Установка isomorphic-dompurify #164
    
    * Функция #164
    
    * Документация #164
    
    * Добавление слов в словарь VS Code

---
+++ src/runtime/composables/useSanitize.ts
@@ -0,0 +1,14 @@
+import { sanitize } from "isomorphic-dompurify"
+
+/**
+ * Данная функция нужна для очистки HTML-кода, полученного от API.
+ *
+ * Благодаря ей код можно использовать в v-html, не боясь XSS-атаки.
+ *
+ * @param html - HTML-код, который необходимо очистить.
+ * @returns очищенный HTML-код.
+ */
+export default function (html: string | undefined) {
+  // Возвращаем очищенный HTML-код
+  return sanitize(html ?? "", { USE_PROFILES: { html: true } })
+}
