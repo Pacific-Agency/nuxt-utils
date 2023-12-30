988dbcd7f762d7dafc8dbcdc09a7e78d9e2f5cb2
Vasily Kuzin <exer7um@gmail.com>
2023-12-30 12:31:04 +0300

    Исправление плагина Vue

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
+import Vue from "@vitejs/plugin-vue"
 import path from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  plugins: [Vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
