b43db0f23bea01156e136b7e06070d487a5f00ba
Vasily Kuzin <exer7um@gmail.com>
2023-12-30 12:25:31 +0300

    Удаление defineModel из конфигурации

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,3 @@
 export default defineNuxtConfig({
   modules: ["../src/module.ts"],
-  vue: {
-    defineModel: true,
-  },
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,7 @@
-import Vue from "@vitejs/plugin-vue"
 import path from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  plugins: [
-    Vue({
-      script: {
-        defineModel: true,
-      },
-    }),
-  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
