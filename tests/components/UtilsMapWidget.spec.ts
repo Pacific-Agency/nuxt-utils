24f22a112efa7b7223d6c96386d1da2726ac781a
Vasily Kuzin <exer7um@gmail.com>
2024-01-15 02:44:27 +0300

    Обновление тестов

--- tests/components/UtilsMapWidget.spec.ts
+++ tests/components/UtilsMapWidget.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from "vitest"
 
 import UtilsMapWidget from "@/runtime/components/UtilsMapWidget.vue"
 
-describe.concurrent("Компонент UtilsMapWidget", () => {
+describe.sequential("Компонент UtilsMapWidget", () => {
   /** `ID` виджета */
   const widgetId = "widget-id"
   /** Список классов */
@@ -24,11 +24,13 @@ describe.concurrent("Компонент UtilsMapWidget", () => {
   })
 
   test("Генерируется правильная ссылка на виджет", () => {
-    // Получение src карты
-    const widgetSrc = wrapper.get("iframe").attributes("src")
+    /** Получение src карты */
+    const widgetURL = new URL(wrapper.get("iframe").attributes("src") ?? "")
 
     // Ожидается правильный src карты
-    expect(widgetSrc).toBe(`https://yandex.ru/map-widget/v1/-/${widgetId}`)
+    expect(widgetURL.href).toBe(
+      `https://yandex.ru/map-widget/v1/-/${widgetId}?z=100`
+    )
   })
 
   test("Присваивание классов с компонента на `iframe`", () => {
@@ -39,17 +41,44 @@ describe.concurrent("Компонент UtilsMapWidget", () => {
     expect(widgetClassList).toContain(classList)
   })
 
+  test("Выставляется параметр z равный 100", async () => {
+    /** Получение src карты */
+    const widgetURL = new URL(wrapper.get("iframe").attributes("src") ?? "")
+
+    /** Параметры URL */
+    const searchParams = widgetURL.searchParams
+
+    // Ожидается правильный параметр z
+    expect(searchParams.get("z")).toBe("100")
+  })
+
   test("Добавляется параметр `lang` в ссылку при передаче локализации", async () => {
     await wrapper.setProps({
       locale: "en",
     })
 
-    // Получение src карты
-    const widgetSrc = wrapper.get("iframe").attributes("src")
+    /** Получение src карты */
+    const widgetURL = new URL(wrapper.get("iframe").attributes("src") ?? "")
 
-    // Ожидается правильный src карты
-    expect(widgetSrc).toBe(
-      `https://yandex.ru/map-widget/v1/-/${widgetId}?lang=en`
-    )
+    /** Параметры URL */
+    const searchParams = widgetURL.searchParams
+
+    // Ожидается правильный параметр lang
+    expect(searchParams.get("lang")).toBe("en")
+  })
+
+  test("Параметр z правильно взаимодействует с lang", async () => {
+    await wrapper.setProps({
+      locale: "en",
+    })
+
+    /** Получение src карты */
+    const widgetURL = new URL(wrapper.get("iframe").attributes("src") ?? "")
+
+    /** Параметры URL */
+    const searchParams = widgetURL.searchParams
+
+    // Ожидается правильные параметры
+    expect(searchParams.toString()).toMatchInlineSnapshot(`"lang=en&z=100"`)
   })
 })
