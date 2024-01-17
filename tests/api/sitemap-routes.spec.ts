5db19471494d55f187798a45dabaefd3a7bf3661
Vasily Kuzin <exer7um@gmail.com>
2024-01-17 15:26:16 +0300

    test: Тестирование эндпоинта `Sitemap` (#189)
    ### 📝 Описание изменений
    Тестирование эндпоинта `Sitemap`.

---
+++ tests/api/sitemap-routes.spec.ts
@@ -0,0 +1,36 @@
+import { $fetch, setup } from "@nuxt/test-utils/e2e"
+import { fileURLToPath } from "node:url"
+import { describe, expect, test } from "vitest"
+
+// @vitest-environment node
+
+describe("Эндпоинт Sitemap", async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL("../fixtures/basic", import.meta.url)),
+  })
+
+  test("Правильный формат данных", async () => {
+    const sitemap = await $fetch("/api/sitemap-routes")
+
+    expect(sitemap).toMatchInlineSnapshot(`
+      [
+        {
+          "_i18nTransform": true,
+          "loc": "entity/slug",
+        },
+        {
+          "_i18nTransform": true,
+          "loc": "entity/second-slug",
+        },
+        {
+          "_i18nTransform": true,
+          "loc": "secondEntity/slug",
+        },
+        {
+          "_i18nTransform": true,
+          "loc": "secondEntity/second-slug",
+        },
+      ]
+    `)
+  })
+})
