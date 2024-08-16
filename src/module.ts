f68b68c08cff517181898b4a52a791c22dac7435
Vasily Kuzin <exer7um@gmail.com>
2024-08-16 16:55:38 +0300

    feat: Авторизация для `nuxt-open-fetch` (#206)
    ### 📝 Описание изменений
    Плагин для добавления авторизации к библиотеке `nuxt-open-fetch`.

--- src/module.ts
+++ src/module.ts
@@ -1,17 +1,32 @@
 import {
   addComponentsDir,
   addImportsDir,
+  addPlugin,
   addServerHandler,
   createResolver,
   defineNuxtModule,
+  hasNuxtModule,
 } from "@nuxt/kit"
+import { consola } from "consola"
 
 export default defineNuxtModule({
+  defaults: {
+    /**
+     * Добавить ли авторизацию для запросов с помощью Nuxt Open Fetch.
+     * Авторизация работает по такому же принципу, как в `useFetchAuth`.
+     *
+     * Для работы данной функции необходимо установить и настроить плагин `nuxt-open-fetch`.
+     *
+     * @default false
+     * @see https://pacific-agency.github.io/nuxt-utils/composables/use-fetch-auth.html
+     */
+    authNuxtOpenFetch: false,
+  },
   meta: {
     configKey: "nuxtUtils",
     name: "@pacific-agency/nuxt-utils",
   },
-  setup() {
+  setup(moduleOptions, { options: nuxtOptions }) {
     const { resolve } = createResolver(import.meta.url)
 
     /** Компоненты */
@@ -27,5 +42,21 @@ export default defineNuxtModule({
       handler: resolve("./runtime/api/sitemap-routes"),
       route: "/api/sitemap-routes",
     })
+
+    /** Плагины */
+    if (moduleOptions.authNuxtOpenFetch) {
+      if (hasNuxtModule("nuxt-open-fetch")) {
+        // @ts-expect-error - свойство openFetch появляется после установки `nuxt-open-fetch`
+        nuxtOptions.openFetch ||= {}
+        // @ts-expect-error - свойство openFetch появляется после установки `nuxt-open-fetch`
+        nuxtOptions.openFetch.disableNuxtPlugin = true
+
+        addPlugin(resolve("./runtime/plugins/openFetch"))
+      } else {
+        consola.warn(
+          "Плагин `nuxt-open-fetch` не установлен. Для работы данной функции необходимо установить и настроить плагин `nuxt-open-fetch`."
+        )
+      }
+    }
   },
 })
